e4f94e4fd3e84be572f3ca8e8ccc57610b2b7787
matheustatangelo <mg-tatangelo@hotmail.com.br>
2023-05-10 16:34:02 -0300

    Alterei alguns consoles no código do App.java

--- src/main/java/questao2/App.java
+++ src/main/java/questao2/App.java
@@ -6,7 +6,7 @@ public class App {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Enter a message: ");
+        System.out.print("Coloque uma mensagem, pode ser sem ou usando um dos emojis: :-) ou :-( : ");
         String message = input.nextLine();
 
         Sentiment sentiment = new Sentiment(message);
